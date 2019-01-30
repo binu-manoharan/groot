837524f6d6dceacaa1f88d9e327ba28b57052f20
binu manoharan <binu1988@gmail.com>
2019-01-30 23:37:09 +0000

    Create a simple hello component

--- groot-web/src/App.tsx
+++ groot-web/src/App.tsx
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Hello from './Hello';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Hello />
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
---
+++ groot-web/src/Hello.tsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Hello extends Component {
+    render() {
+        return (
+            <div>hello there!</div>
+        );
+      }
+}
+
+export default Hello;
